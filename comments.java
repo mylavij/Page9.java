358b432cafd1e392389b58adb76f903c177e95c6
mylavij <mvijayan@stu.socsd.org>
2024-02-07 14:51:36 -0500

    yy

---
+++ comments.java
@@ -0,0 +1,15 @@
+/*****************
+*Project: LabOneA
+*Programmer: John Smith 
+*Date: September 23
+*Program Name: Hello.java
+*****************/
+public class comments
+{
+    public static void main (String[] args)
+    {
+        System.out.println("This is a literal print.");
+        System.out.print("Yea!");
+        System.out.println("Go Java!");
+    }
+}
