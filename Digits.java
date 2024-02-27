bd4f440246245c428fdfa6b4a1b82cd252996bf7
mylavij <mvijayan@stu.socsd.org>
2024-02-27 14:59:20 -0500

    jj

---
+++ Digits.java
@@ -0,0 +1,12 @@
+public class Digits
+{
+    public static void main (String[] args)
+    {
+        double hundreds = 2 
+
+        System.out.println("The hundreds-place digit is: " + double hundreds);
+        System.out.println("The tens-place digit is: ");
+        System.out.println("The one's digit is: ");
+
+    }
+}
