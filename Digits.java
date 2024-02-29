4cb412859a55b82e64e61e69dcd242adff4577de
mylavij <mvijayan@stu.socsd.org>
2024-02-29 14:49:09 -0500

    yy
    yy

--- Digits.java
+++ Digits.java
@@ -2,9 +2,11 @@ public class Digits
 {
     public static void main (String[] args)
     {
-        double hundreds = 2 
-
-        System.out.println("The hundreds-place digit is: " + double hundreds);
+        int num = 258
+        ones digit = num % 10
+        tens digit = num % 100
+        hundreds digit = num % 100/100
+        System.out.println("The hundreds-place digit is: " + hundreds digit);
         System.out.println("The tens-place digit is: ");
         System.out.println("The one's digit is: ");
 
